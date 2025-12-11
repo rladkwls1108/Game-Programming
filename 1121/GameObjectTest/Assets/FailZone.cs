854723492c83796d99c80350c057c551a58cfd34
DOI <81212694+rladkwls1108@users.noreply.github.com>
2025-12-11 22:02:19 +0900

    Add files via upload

---
+++ 1121/GameObjectTest/Assets/FailZone.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class FailZone : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        //Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.name == "Ball")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
